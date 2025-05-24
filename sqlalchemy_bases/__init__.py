70d912803ac0d2ab368552f8dbd46838ccd73555
Liam Whitenack <liamwhitenack@outlook.com>
2025-05-24 11:36:40 -0400

    part of last commit

--- sqlalchemy_bases/__init__.py
+++ sqlalchemy_bases/__init__.py
@@ -1,5 +1,6 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Table, Text
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 
 class Base(DeclarativeBase):
