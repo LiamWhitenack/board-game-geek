302a769530f31965723dd180c1f0445413c64e61
Liam Whitenack <liamwhitenack@outlook.com>
2026-02-24 13:07:47 -0500

    a fix from so long ago I don't remember what it does

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from fetch.get_from_api import get_from_bgg
 from sql.game import Game
 from sql.session import session
 
-for i in range(1, 10_000):
+at_least_one_added = [True for _ in range(10)]
+for i in range(1, 100_000):
     print(i)
     j = 20 * i
-    at_least_one_added = False
+    add_one = False
     for base in get_from_bgg(range(j + 1, j + 21)):
-        at_least_one_added = True
+        add_one = True
         session.merge(base)
-    if not at_least_one_added:
+
+    del at_least_one_added[0]
+    at_least_one_added.append(add_one)
+    if not any(at_least_one_added):
         break
     session.commit()
     sleep(2)
